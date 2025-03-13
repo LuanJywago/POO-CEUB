c168bb51b9a14c0102866744123c95c6c6a4d851
JywagoB&F <luan.jywago@sempreceub.com>
2025-03-13 10:43:26 -0300

    metodos de incremento e decremento

---
+++ IncremDecrem.java
@@ -0,0 +1,6 @@
+public class IncremDecrem {
+    public static void main(String[] args) {
+        Incrementa.incrementa();
+        Decrementa.decrementa();
+    }
+}
---
+++ Incrementa.java
@@ -0,0 +1,18 @@
+public class Incrementa
+{
+    public static void incrementa() {
+        System.out.println("Vamos incrementar em 1 e 1");
+        // Usando incremento ctd = ctd + 1
+        for (int ctd = 1; ctd <= 10; ctd = ctd + 1) {
+            System.out.println("ctd = ctd + 1: " + ctd);
+        }
+        // Usando incremento ctd += 1
+        for (int ctd = 1; ctd <= 10; ctd += 1) {
+            System.out.println("ctd += 1: " + ctd);
+        }
+        // Usando incremento ctd ++
+        for (int ctd = 1; ctd <= 10; ctd ++) {
+            System.out.println("ctd ++: " + ctd);
+        } 
+    }
+}
