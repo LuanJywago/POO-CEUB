a346f873287ba53be3797c68e6344653b41d0363
JywagoB&F <luan.jywago@sempreceub.com>
2025-05-29 08:12:49 -0300

    Fornecedor.java

--- AVA2-PESSOAS/Fornecedor.java
+++ AVA2-PESSOAS/Fornecedor.java
@@ -9,7 +9,7 @@ public class Fornecedor extends Pessoa {
     public String getProdutoFornecido() { return produtoFornecido; }
     public void setProdutoFornecido(String produtoFornecido) { this.produtoFornecido = produtoFornecido; }
 
-    @Override
+
     public void exibirInfo() {
         super.exibirInfo();
         System.out.println("Produto Fornecido: " + produtoFornecido);
