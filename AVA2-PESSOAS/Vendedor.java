234816a80c8f29f9bb31dbdcd93c5081d93f7bb5
JywagoB&F <luan.jywago@sempreceub.com>
2025-05-29 08:13:55 -0300

    POO-CEUB-QUI-Luan Jywago Santos Ferreira

--- AVA2-PESSOAS/Vendedor.java
+++ AVA2-PESSOAS/Vendedor.java
@@ -6,10 +6,15 @@ public class Vendedor extends Pessoa {
         this.comissao = comissao;
     }
 
-    public double getComissao() { return comissao; }
-    public void setComissao(double comissao) { this.comissao = comissao; }
+    public double getComissao() { 
+        return comissao;
+    }
+    
+    public void setComissao(double comissao) { 
+        this.comissao = comissao;
+    }
 
-    @Override
+
     public void exibirInfo() {
         super.exibirInfo();
         System.out.println("Comissão: " + comissao);
